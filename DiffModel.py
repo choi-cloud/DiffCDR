4a2f568e0f2dbaccec927315c5e82f06dd7ae39f
na1-4an <parkne0114@gmail.com>
2026-02-10 03:02:22 +0000

    DiffCDR 클래스, 함수 추가

--- DiffModel.py
+++ DiffModel.py
@@ -36,6 +36,82 @@ def get_timestep_embedding(timesteps, embedding_dim: int):
     return emb
 
 
+class DiffCDR(nn.Module):
+    def __init__(self,num_steps=200, diff_dim=32,input_dim =32,c_scale=0.1,diff_sample_steps=30,diff_task_lambda=0.1,diff_mask_rate=0.1 ):
+        super(DiffCDR,self).__init__()
+
+        #-------------------------------------------
+        #define params
+        self.num_steps = num_steps
+        self.betas = torch.linspace(1e-4,0.02 ,num_steps)
+
+        self.alphas = 1-self.betas
+        self.alphas_prod = torch.cumprod(self.alphas,0)
+        self.alphas_prod_p = torch.cat([torch.tensor([1]).float(),self.alphas_prod[:-1]],0)
+        self.alphas_bar_sqrt = torch.sqrt(self.alphas_prod)
+        self.one_minus_alphas_bar_log = torch.log(1 - self.alphas_prod)
+        self.one_minus_alphas_bar_sqrt = torch.sqrt(1 - self.alphas_prod)
+
+        assert self.alphas.shape==self.alphas_prod.shape==self.alphas_prod_p.shape==\
+        self.alphas_bar_sqrt.shape==self.one_minus_alphas_bar_log.shape\
+        ==self.one_minus_alphas_bar_sqrt.shape
+
+        #-----------------------------------------------
+        self.diff_dim = diff_dim
+        self.input_dim = input_dim
+        self.task_lambda = diff_task_lambda
+        self.sample_steps = diff_sample_steps
+        self.c_scale = c_scale
+        self.mask_rate = diff_mask_rate
+        #-----------------------------------------------
+        
+        self.linears = nn.ModuleList(
+            [
+                nn.Linear(input_dim,diff_dim),    
+                nn.Linear(diff_dim,diff_dim) ,     
+                nn.Linear(diff_dim,input_dim),  
+            ]
+        )
+        
+        self.step_emb_linear = nn.ModuleList(
+            [   
+                nn.Linear(diff_dim,input_dim),
+            ]
+        )
+
+        self.cond_emb_linear = nn.ModuleList(
+            [   
+                nn.Linear(input_dim,input_dim),
+            ]
+        ) 
+
+        self.num_layers = 1
+
+        #linear for alm 
+        self.al_linear = nn.Linear(input_dim,input_dim,False)
+
+    def forward(self, x,t, cond_emb,cond_mask ):
+
+        for idx in range( self.num_layers ):
+        
+            t_embedding = get_timestep_embedding( t , self.diff_dim)
+            t_embedding = self.step_emb_linear[idx](t_embedding)
+        
+            cond_embedding = self.cond_emb_linear[idx](cond_emb)
+        
+            t_c_emb = t_embedding + cond_embedding * cond_mask.unsqueeze(-1)
+            x = x + t_c_emb
+            #x= torch.cat([t_embedding,cond_embedding * cond_mask.unsqueeze(-1),x],axis=1)
+
+            x = self.linears[0](x) 
+            x = self.linears[1](x) 
+            x = self.linears[2](x) 
+
+        return x
+        
+    def get_al_emb(self,emb):
+        return self.al_linear (emb)
+
 class SinusoidalPositionEmbeddings(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -196,6 +272,48 @@ def q_x_fn(model, x_0, t, device):  # forward
 
     return (alphas_t * x_0 + alphas_1_m_t * noise), noise  # x0에 노이즈를 더함.
 
+def diffusion_loss_fn(model,x_0,cond_emb, iid_emb,y_input,
+                        device,is_task):
+
+    num_steps = model.num_steps
+    mask_rate = model.mask_rate
+
+    if is_task == False:
+
+        #------------------------
+        #sampling
+        #------------------------
+        batch_size = x_0.shape[0]
+        #sample t
+        t = torch.randint(0,num_steps,size=(batch_size//2,),device=device)
+        if batch_size%2 ==0:
+            t = torch.cat([t,num_steps-1-t],dim=0)
+        else:
+            extra_t = torch.randint(0,num_steps,size=(1,),device=device)
+            t = torch.cat([t,num_steps-1-t,extra_t],dim=0)
+        t = t.unsqueeze(-1)
+
+        x,e = q_x_fn(model,x_0,t,device)
+        
+        #random mask
+        cond_mask = 1 * (torch.rand(cond_emb.shape[0],device=device) <= mask_rate  )
+        cond_mask = 1 - cond_mask.int()
+
+        #pred noise
+        output = model(x, t.squeeze(-1),cond_emb,cond_mask )
+
+        return F.smooth_l1_loss(e, output)
+
+    elif is_task:
+        final_output,iid_emb=p_sample_loop(model,cond_emb,iid_emb,device)
+        y_pred = torch.sum( final_output * iid_emb , dim=1)
+        
+        #MSE
+        task_loss =   (y_pred - y_input.squeeze().float()).square().mean()
+        #RMSE
+        #task_loss =   (y_pred - y_input.squeeze().float()).square().sum().sqrt() / y_pred.shape[0]
+
+        return F.smooth_l1_loss(x_0, final_output) + model.task_lambda* task_loss
 
 def diffusion_loss_fn_parallel(
     model, x_0_m, x_0_g, cond_emb1, cond_emb2, iid_emb, y_input, device, is_task, q_embs1=None, q_embs2=None, style_src=None, uid=None, iid=None
@@ -413,3 +531,49 @@ def p_sample_parallel(model, cond_emb, x, iid_emb, device, diff_id):
 def p_sample_loop_parallel(model, start_emb, cond_emb, iid_input, device, diff_id):
     cur_x, iid_emb_out = p_sample_parallel(model=model, cond_emb=cond_emb, x=start_emb, iid_emb=iid_input, device=device, diff_id=diff_id)
     return cur_x, iid_emb_out
+
+#generation fun
+def p_sample(model,cond_emb,x,iid_emb,device):
+    #wrap for dpm_solver
+    classifier_scale_para = model.c_scale
+    dmp_sample_steps = model.sample_steps
+    num_steps = model.num_steps
+
+    model_kwargs ={'cond_emb':cond_emb,
+                'cond_mask':torch.zeros( cond_emb.size()[0] ,device=device),
+                }
+
+
+    model_fn = model_wrapper(
+        model,
+        noise_schedule,
+        is_cond_classifier=True,
+        classifier_scale = classifier_scale_para, 
+        time_input_type="1",
+        total_N=num_steps,
+        model_kwargs=model_kwargs
+    )
+
+    dpm_solver = DPM_Solver(model_fn, noise_schedule)
+
+    sample = dpm_solver.sample(
+                    x,
+                    steps=dmp_sample_steps,
+                    eps=1e-4,
+                    adaptive_step_size=False,
+                    fast_version=True,
+                )
+    
+    return model.get_al_emb(sample).to(device),iid_emb
+
+
+def p_sample_loop(model,cond_emb,iid_input,device): 
+    #source emb input 
+    cur_x = cond_emb
+    #noise input 
+    #cur_x = torch.normal(0,1,size = cond_emb.size() ,device=device)
+
+    #reversing
+    cur_x,iid_emb_out = p_sample(model,cond_emb,cur_x,iid_input,device)
+
+    return cur_x ,iid_emb_out
